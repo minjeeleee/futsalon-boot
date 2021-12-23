8a967aefda8321f053f7b57ca3e313f625f842a9
minjeeleee <fly12035@gmail.com>
2021-12-23 23:46:52 +0900

    base project

---
+++ src/main/java/com/footsalon/FootsalonApplication.java
@@ -0,0 +1,14 @@
+package com.footsalon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class FootsalonApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FootsalonApplication.class, args);
+	}
+
+}
