e3f075f84cd338d73d5afe3e86203c6618304b43
h0neyalpaca <dalcom0_0@naver.com>
2021-12-26 22:06:51 +0900

    [서우경] Add Entity

---
+++ src/main/java/com/footsalon/teamPenalty/TeamPenalty.java
@@ -0,0 +1,12 @@
+package com.footsalon.teamPenalty;
+
+import com.footsalon.member.Member;
+
+import java.time.LocalDateTime;
+
+public class TeamPenalty {
+
+    private Long tpIdx;
+    private Member member;
+    private LocalDateTime delDate;
+}
