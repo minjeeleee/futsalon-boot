1efa450fe2d9f214e57cb09cb24b4f38730b00ce
myajit <myajit@naver.com>
2021-12-29 03:55:03 +0900

    jung login modi

---
+++ src/main/java/com/footsalon/member/controlller/MemberController.java
@@ -0,0 +1,18 @@
+package com.footsalon.member.controlller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("member")
+public class MemberController {
+
+
+    @GetMapping("login")
+    public void login() {
+    }
+
+}
